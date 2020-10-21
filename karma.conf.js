50527669df1843ce0009a33fa6b9904a5845f42a
Chris Keller <xylo04@gmail.com>
2020-10-21 12:55:12 -0600

    Update project name

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/UploadDownloadPOC'),
+      dir: require('path').join(__dirname, './coverage/AngularClientUploadDownload'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'UploadDownloadPOC'`, () => {
+  it(`should have as title 'AngularClientUploadDownload'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('UploadDownloadPOC');
+    expect(app.title).toEqual('AngularClientUploadDownload');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('UploadDownloadPOC app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('AngularClientUploadDownload app is running!');
   });
 });
